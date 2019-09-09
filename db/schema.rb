186f644097b772557d1a2b0b97c49ac0be35a3cb
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-09-09 19:17:50 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190909185138) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.string "password_digest"
+    t.decimal "balance"
+  end
 
 end
