7c87e63fd5f6d3029e9d13387fb17f1c45a94112
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-09-09 19:17:36 +0000

    Automatically backed up by Learn

--- db/migrate/20190909185138_users.rb
+++ db/migrate/20190909185138_users.rb
@@ -3,6 +3,7 @@ class Users < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :username
       t.string :password
+      t.string :password_digest
       t.decimal :balance
     end
   end
